a348f6e83b68941d39db450a621a080770b3549b
Hannes <h@nnes.dev>
2026-09-14 17:47:18 +0800

    Release v0.0.2

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
             roc-vscode-vsix = pkgs.stdenv.mkDerivation (finalAttrs: {
               name = "roc-vscode.vsix";
               pname = "roc-vscode-vsix";
-              version = "0.0.1";
+              version = "0.0.2";
 
               src = pkgs.lib.cleanSource ./.;
               sourceRoot = "source";
